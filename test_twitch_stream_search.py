7de7f3740f5472f4d5b9f1a20c444cd00bae16a0
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2025-01-23 22:52:24 +0300

    fx

--- test_twitch_stream_search.py
+++ test_twitch_stream_search.py
@@ -3,31 +3,38 @@ import random
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_twitch_stream_search(driver):
     # 1. Go to Twitch mobile site
     driver.get("https://m.twitch.tv/")
-    time.sleep(3)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "body"))
+    )
 
     # 1.1 Accept Cookies if the banner appears
     try:
-        cookie_banner_accept = driver.find_element(By.XPATH, "//div[text()='Accept']")
+        cookie_banner_accept = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, "//div[text()='Accept']"))
+            )
         cookie_banner_accept.click()
-        time.sleep(2)
     except:
         print("No cookie banner found, proceeding...")
 
     # 2. Click on the search icon
-    search_icon = driver.find_element(By.XPATH, "//div[text()='Browse']")
+    search_icon = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='Browse']"))
+        )
     search_icon.click()
-    time.sleep(2)
 
     # 3. Input "StarCraft II"
-    search_input = driver.find_element(By.CSS_SELECTOR, "input[type='search']")
+    search_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='search']"))
+        )
     search_input.send_keys("StarCraft II")
     search_input.send_keys(Keys.ENTER)
-    time.sleep(3)
 
     # 4. Scroll down 2 times
     actions = ActionChains(driver)
@@ -36,12 +43,15 @@ def test_twitch_stream_search(driver):
         time.sleep(2)
 
     # 5. Select a random streamer
-    main_content = driver.find_element(By.ID, "page-main-content-wrapper")
+    main_content = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "page-main-content-wrapper"))
+    )
     last_section = main_content.find_element(By.XPATH, "./div/div/section[last()]")
     random_video_div = last_section.find_element(By.XPATH, f"./div[{random.randint(2, 3)}]")
     random_video_div.click()
 
     # 6. Wait for the streamer's page to load completely and take a screenshot
-    time.sleep(10)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "video"))
+    )
     driver.save_screenshot("streamer_page_screenshot.png")
-    print("Screenshot saved as 'streamer_page_screenshot.png'")
