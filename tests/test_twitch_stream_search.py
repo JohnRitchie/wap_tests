cef454c75e139ed407e6ff57a5cc3ab3b6d476ab
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2025-01-24 17:49:25 +0300

    parametrize

--- tests/test_twitch_stream_search.py
+++ tests/test_twitch_stream_search.py
@@ -1,19 +1,21 @@
 import allure
+import pytest
 
 
-@allure.suite("Twitch Stream Ssearch")
-class TestTwitchStreamSsearch:
+@allure.suite("Twitch Stream Search")
+class TestTwitchStreamSearch:
     @allure.severity(allure.severity_level.BLOCKER)
     @allure.title("Check the basic functionality of the twitch search")
-    def test_twitch_stream_search(self, twitch_main_page):
+    @pytest.mark.parametrize("game_name", ["StarCraft II"])
+    def test_twitch_stream_search(self, twitch_main_page, game_name):
         with allure.step("Accept Cookies if the banner appears"):
             twitch_main_page.accept_cookies()
 
         with allure.step("Click on the search icon"):
             twitch_main_page.click_browse()
 
-        with allure.step("Input 'StarCraft II'"):
-            twitch_main_page.search_game("StarCraft II")
+        with allure.step(f"Input '{game_name}'"):
+            twitch_main_page.search_game(game_name)
 
         with allure.step("Scroll down 2 times"):
             twitch_main_page.scroll_down(2)
@@ -22,4 +24,4 @@ class TestTwitchStreamSsearch:
             twitch_main_page.select_random_video()
 
         with allure.step("Wait for the streamer's page to load completely and take a screenshot"):
-            twitch_main_page.wait_for_video_and_screenshot("streamer_page_screenshot.png")
+            twitch_main_page.wait_for_video_and_screenshot(f"{game_name} streamer page screenshot.png")
